1ed11f288d4f51dcc4ee4cc635470400868b4ee7
Althea <mujar.altheadanielle@gmail.com>
2026-08-13 20:29:52 +0800

    Added test for Forgot Password function

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 class LoginPage:
     def __init__(self, page: Page):
@@ -8,7 +8,23 @@ class LoginPage:
         self.loginButton = page.get_by_role("button", name="Login")
         self.alertMessage = page.get_by_role("alert")
         
+        self.forgotPassword = page.get_by_text("Forgot your password?")
+        self.cancelButton = page.get_by_role("button", name="Cancel")
+        self.resetpasswordButton = page.get_by_role("button", name="Reset Password")
+
     def login(self, username: str, password: str):
         self.usernameInput.fill(username)
         self.passwordInput.fill(password)
         self.loginButton.click()
+        
+    def navigate_forgot_password(self):
+        self.forgotPassword.click()
+        self.page.wait_for_url("**/auth/requestPasswordResetCode")
+    
+    def submit_reset_password(self, username: str):
+        expect(self.usernameInput).to_be_visible
+        
+        self.usernameInput.fill(username)
+        self.resetpasswordButton.click()
+        
+        self.page.wait_for_url("**/auth/sendPasswordReset")
--- tests/test_LoginPage.py
+++ tests/test_LoginPage.py
@@ -20,3 +20,28 @@ def test_failed_login(page:Page):
     
     expect(login_page.alertMessage).to_be_visible()
     expect(login_page.alertMessage).to_have_text("Invalid credentials")
+    
+
+def test_forgot_password(page:Page):
+    login_page = LoginPage(page)
+    
+    page.goto("/")
+    
+    login_page.navigate_forgot_password()
+    
+    login_page.submit_reset_password("test_user")
+    
+    expect(page.get_by_text("Reset Password link sent successfully")).to_be_visible()
+    
+def test_cancel_forgot_password(page:Page):
+    login_page = LoginPage(page)
+        
+    page.goto("/")
+    
+    login_page.navigate_forgot_password()
+    
+    login_page.cancelButton.click()
+    
+    page.wait_for_url("**/auth/login")
+    
+    expect(page.get_by_role("heading", name="Login")).to_be_visible()
