3ce0fa4822ea03effb9e3629975d92396578cee6
Althea <mujar.altheadanielle@gmail.com>
2026-08-19 13:07:30 +0800

    Added a file to store the credentials

--- conftest.py
+++ conftest.py
@@ -1,6 +1,10 @@
+import os
 import pytest
+from dotenv import load_dotenv
 from playwright.sync_api import Page
 from pages.login_page import LoginPage
+
+load_dotenv()
 
 @pytest.fixture(autouse=True)
 def go_to_login(page: Page):
@@ -10,7 +14,9 @@ def go_to_login(page: Page):
 @pytest.fixture
 def logged_in_page(page:Page):
     login_page = LoginPage(page)
-    login_page.login("Admin", "admin123")
+    username = os.environ["ADMIN_USERNAME"]
+    password = os.environ["ADMIN_PASSWORD"]
+    login_page.login(username, password)
     
     page.wait_for_url("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
     page.wait_for_selector("h6", timeout=10000)
